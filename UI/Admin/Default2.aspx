0c99dc9e96171fb363505ca9ae41921be0c62765
bfchengnuo <bfchengnuo@gmail.com>
2016-09-26 16:18:15 +0800

    初始化

---
+++ UI/Admin/Default2.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/MasterPage.master" AutoEventWireup="true" CodeFile="Default2.aspx.cs" Inherits="Admin_Default2" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <p>这是本校教师</p>
+</asp:Content>
+
---
+++ UI/Test/MasterPage.master.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+public partial class MasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //测试无法转换为DataSet
+        //DataSet ds = (DataSet)SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+        //dt = ds.Tables[0];
+        DataView ds = (DataView)SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+        DataTable dt = new DataTable();
+        dt = ds.Table;
+        TreeNode root = new TreeNode(dt.Rows[0][1].ToString());//定义一个根节点
+        root.NavigateUrl = dt.Rows[0][2].ToString();
+        TreeNode softdipartment = null;
+        for (int i = 1; i < dt.Rows.Count; i++)
+        {
+            int no = (int)dt.Rows[i][0];
+            if (no == 3 && softdipartment != null)
+            {
+                TreeNode netdirection = new TreeNode(dt.Rows[i][1].ToString());
+                netdirection.NavigateUrl = dt.Rows[i][2].ToString();
+                softdipartment.ChildNodes.Add(netdirection);//将子节点添加到父节(softdipartment)点中
+            }
+            else
+            {
+                softdipartment = new TreeNode(dt.Rows[i][1].ToString());
+                softdipartment.NavigateUrl = dt.Rows[i][2].ToString();
+                root.ChildNodes.Add(softdipartment);//将子节点添加到父节点中
+            }
+        }
+        this.TreeView1.Nodes.Add(root);//将根节点添加到treeview中（就将根节点及子节点都装到了treeview中）
+
+    }
+
+    protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+    {
+
+    }
+}
