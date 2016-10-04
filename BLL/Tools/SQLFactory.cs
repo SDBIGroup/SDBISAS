301cf403bc309cd5c54d518ccf6d41a9735bda6c
bfchengnuo <bfchengnuo@gmail.com>
2016-10-04 14:01:34 +0800

    增加登陆跳转、BLL层查询逻辑

---
+++ BLL/Tools/SQLFactory.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 用于将参数封装成SQL语句返回
+/// </summary>
+namespace BLL.Tools
+{
+    class SQLFactory
+    {
+        //关于查询语句的各种重载
+
+        /// <summary>
+        /// 查询一整张表
+        /// </summary>
+        /// <param name="Tname">表名</param>
+        /// <returns></returns>
+        public static string BuildSQLSelectString(string Tname)
+        {
+            return "select * from " + Tname;
+        }
+
+        /// <summary>
+        /// 返回有一个条件限制的SQL语句
+        /// </summary>
+        /// <param name="Tname">表名</param>
+        /// <param name="dl">条件名</param>
+        /// <param name="text">条件值</param>
+        /// <returns></returns>
+        public static string BuildSQLSelectString(string Tname, string dl, string text)
+        {
+            return "select * from " + Tname + " where " + dl + "='" + text + "'";
+        }
+
+
+        /// <summary>
+        /// 查询指定表，两个条件限制
+        /// </summary>
+        /// <param name="Tname">表名</param>
+        /// <param name="dl">条件一名</param>
+        /// <param name="d2">条件二名</param>
+        /// <param name="text1">条件一值</param>
+        /// <param name="text2">条件二值</param>
+        /// <returns></returns>
+        public static string BuildSQLSelectString(string Tname, string dl, string d2, string text1, string text2)
+        {
+            return "select * from " + Tname + " where " + dl + "='" + text1 + "' and " + d2 + "='" + text2 + "'";
+        }
+
+        /// <summary>
+        /// 查询指定列的数据
+        /// </summary>
+        /// <param name="column">列名</param>
+        /// <param name="Tname">表名</param>
+        /// <returns></returns>
+        public static string BuildSQLSelectString(string column, string Tname)
+        {
+            return "select " + column + " from " + Tname;
+        }
+    }
+}
--- UI/Login.aspx
+++ UI/Login.aspx
@@ -8,5 +8,11 @@
     <title></title>
 </head>
 <body>
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    </form>
     </body>
 </html>
