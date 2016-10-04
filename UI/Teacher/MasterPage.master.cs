b426787e7604f554ecdc77b412d691537dbf67ab
kiuber <582279412@qq.com>
2016-10-04 13:35:34 +0800

    添加连接校园网数据库

--- UI/Teacher/MasterPage.master.cs
+++ UI/Teacher/MasterPage.master.cs
@@ -10,6 +10,7 @@ public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
         if (!IsPostBack)
         {
             loadData2TreeView();
