e8da3ee9238f970ecac562b2fc7994db6256a976
bfchengnuo <bfchengnuo@gmail.com>
2016-10-27 10:15:18 +0800

    更新部分导入Excel到数据库的代码

--- BLL/ExcelToDatabase.cs
+++ BLL/ExcelToDatabase.cs
@@ -6,10 +6,64 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
+    /// <summary>
+    /// Excel导入到数据库的中转层
+    /// 负责检测文件的合法性以及调用DAL层导入到数据库
+    /// </summary>
     public class ExcelToDatabase
     {
-        public static string CheckFile(string fileName,string identity)
+        /// <summary>
+        /// 检测文件名是否符合Excel后缀规范，以及文件的大小
+        /// 通过后上传到数据库的指定表
+        /// </summary>
+        /// <param name="fileName">文件完整路径包括后缀</param>
+        /// <param name="identity">要导入到的表名</param>
+        /// <returns>错误信息或者成功信息</returns>
+        public static string CheckFile(string fileName, string identity)
         {
+            try
+            {
+                if (fileName != string.Empty)
+                {
+                    //TODO 检测文件大小感觉有问题
+                    if (fileName.Length == 0)
+                    {
+                        return "导入的Excel文件大小为0，请检查！";
+                    }
+
+                    //获取选择文件的扩展名
+                    string fileExtenSion = fileName.Substring(fileName.LastIndexOf(".") + 1);
+                    //检测文件扩展名(格式)
+                    if (fileExtenSion.ToLower() != ".xls" && fileExtenSion.ToLower() != ".xlsx")
+                    {
+                        return "上传的文件格式不正确";
+                    }
+
+                    return ToSQLServer(fileName, identity);
+                }
+                else
+                {
+                    return "选择的文件为空，请重新选择！";
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //TODO
+        public static string ToSQLServer(string fileName, string identity)
+        {
+            if (identity == "TabTeachers" || identity == "TabOtherTeachers")
+            {
+                //return DAL.ExcelToSQLServer.ReadTeachersExcel(fileName, identity);
+            }
+            else
+            {
+
+            }
+
             return "";
         }
     }
---
+++ DAL/ExcelToSQLServer.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Configuration;
+
+namespace DAL
+{
+    public class ExcelToSQLServer
+    {
+        public static DataSet ds;
+
+        /// <summary>
+        /// 检查教师Excel中的列是否符合规范
+        /// </summary>
+        /// <returns>规范返回true，不规范返回false</returns>
+        private static bool CheckExcelTableTeachers()
+        {
+            try
+            {
+                string[] str = { "部门", "工号", "密码", "姓名", "性别", "权限" };
+                for (int i = 0; i <= 5; i++)
+                {
+                    if (ds.Tables["ExcelInfo"].Columns[i].ColumnName.ToString() != str[i])
+                        return false;
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 检查校历Excel中的列是否符合规范
+        /// </summary>
+        /// <returns>规范返回true，不规范返回false</returns>
+        private static bool CheckExcelTableCalendar()
+        {
+            try
+            {
+                string[] str = { "周次", "起", "止" };
+                for (int i = 0; i <= 2; i++)
+                {
+                    if (ds.Tables["ExcelInfo"].Columns[i].ColumnName.ToString() != str[i])
+                        return false;
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// 检查课程Excel中的列是否符合规范
+        /// </summary>
+        /// <returns>规范返回true，不规范返回false</returns>
+        private static bool CheckExcelTableCourses()
+        {
+            try
+            {
+                string[] str = { "承担单位", "任课教师", "上课时间/地点", "课程", "所属部门" };
+                for (int i = 0; i <= 4; i++)
+                {
+                    if (ds.Tables["ExcelInfo"].Columns[i].ColumnName.ToString() != str[i])
+                        return false;
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 获取读取Excel引擎的连接
+        /// </summary>
+        /// <param name="fileName">Excel表的存放位置</param>
+        /// <returns>拼接好的连接</returns>
+        private static OleDbConnection getOleConn(string fileName)
+        {
+            System.GC.Collect();
+            OleDbConnection oleConn;
+
+            //HDR=Yes，这代表第一行是标题，不做为数据使用 ，如果用HDR=NO，则表示第一行不是标题，做为数据来使用。系统默认的是YES  
+            string connstr2003 = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName + ";Extended Properties='Excel 8.0;HDR=Yes;IMEX=1;'";
+            string connstr2007 = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties=\"Excel 12.0;HDR=YES\"";
+
+            string fileExtenSion = fileName.Substring(fileName.LastIndexOf(".") + 1);
+            //建立连接，根据不同的扩展名，选择不同的引擎
+            if (fileExtenSion.ToLower() == ".xls")
+            {
+                oleConn = new OleDbConnection(connstr2003);
+            }
+            else
+            {
+                oleConn = new OleDbConnection(connstr2007);
+            }
+            return oleConn;
+        }
+
+        /// <summary>
+        /// 读取Excel表的数据，并将数据插入到数据库的TabAllCourses表中
+        /// </summary>
+        /// <param name="fileName">Excel表的存放位置</param>
+        /// <param name="strSQL">操作Excel表的sql语句</param>
+        private static void ExcelToDatabaseByDataReader(string fileName, string strSQL)
+        {
+            OleDbConnection oleConn = getOleConn(fileName);
+            oleConn.Open();
+
+            OleDbCommand oleCmd = new OleDbCommand(strSQL, oleConn);
+            OleDbDataReader oleDr = oleCmd.ExecuteReader();
+
+            //进行数据库连接
+            string sqlStr1 = ConfigurationManager.ConnectionStrings["SdbiAttentionSystemConnectionString"].ConnectionString;
+            SqlConnection sqlConn = new SqlConnection(sqlStr1);
+            sqlConn.Open();
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.Connection = sqlConn;
+
+            StringBuilder strconn = new StringBuilder();
+            List<string> str = new List<string>();
+            while (oleDr.Read())
+            {
+                //str = SplitTeacherIDAndTeacherName(oleDr[1].ToString());
+                strconn.Append("insert into TabAllCourses(TeacherDapartment,TeacherID,TeacherName,TimeAndArea,Course,t1,t2,t3,Class,StudentDepartment,StudentID,StudentName,t4,t5,t6,t7)values(");
+                strconn.Append("'" + oleDr[0].ToString() + "','" + str[0] + "','" + str[1] + "'");
+                for (int j = 2; j <= 14; j++)
+                {
+                    strconn.Append(",'" + oleDr[j].ToString() + "'");
+                }
+                strconn.Append(")");
+                sqlCmd.CommandText = strconn.ToString();
+                sqlCmd.ExecuteNonQuery();
+                strconn.Remove(0, strconn.Length);
+            }
+
+
+            sqlConn.Close();
+            oleDr.Close();
+            oleConn.Close();
+        }
+
+        /// <summary>
+        /// 获取Excel工作簿中表的名称
+        /// 顺序不确定
+        /// </summary>
+        /// <param name="fileName">Excel的完整路径</param>
+        /// <returns>含有表名的list</returns>
+        private static List<string> GetSheetName(string fileName)
+        {
+            OleDbConnection conn = getOleConn(fileName);
+            conn.Open();
+
+            List<string> SheetNameList = new List<string>();
+            DataTable dtExcelSchema = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+            string SheetName = "";
+            for (int i = 0; i < dtExcelSchema.Rows.Count; i++)
+            {
+                SheetName = dtExcelSchema.Rows[i]["TANLE_NAME"].ToString();
+                SheetNameList.Add(SheetName);
+            }
+            conn.Close();
+            conn.Dispose();
+            return SheetNameList;
+        }
+
+        /// <summary>
+        /// 获取Excel表的信息
+        /// 将装到ds中
+        /// </summary>
+        /// <param name="fileName">Excel的完整路径</param>
+        /// <param name="strSQL">要执行的SQL语句</param>
+        public static void ReadExcelToDataSet(string fileName, string strSQL)
+        {
+            OleDbConnection conn = getOleConn(fileName);
+            conn.Open();
+
+            OleDbDataAdapter da = new OleDbDataAdapter(strSQL, conn);
+            da.SelectCommand.CommandTimeout = 600;
+
+            ds = new DataSet();
+            da.Fill(ds, "ExcellInfo");
+
+            conn.Close();
+            conn.Dispose();
+        }
+
+        /// <summary>
+        /// 读取课程信息的Excel表
+        /// </summary>
+        /// <param name="fileName">Excel表的完整路径</param>
+        /// <param name="identity">表名【非工作簿名】</param>
+        /// <returns>错误/成功信息</returns>
+        public static string ReadCoursesExcel(string fileName, string identity)
+        {
+            List<string> SheetName = new List<string>();
+            SheetName = GetSheetName(fileName);
+
+            string strSQL = "";
+            if (SheetName[0] != identity + "$")
+            {
+                return "指定的Excel文件的工作表名不为" + identity + ",当前的表名为" + SheetName[0];
+            }
+            strSQL = "select * from [" + SheetName[0] + "]";
+            ReadExcelToDataSet(fileName, strSQL);
+            if (CheckExcelTableCourses())
+            {
+                //CoursesTOSQLServer(identity);
+                return "文件导入成功";
+            }
+            else
+                return "选择的Excel文件中的内容与数据库要求不匹配。请确认！";
+        }
+    }
+}
